fb21a36586878885b9ad9c1a2718150ea18bcbce
Alex <alex.holder@bigfootds.com>
2024-07-18 11:24:43 +1000

    feat: rendering content from localstorage

--- src/pokemonEncounters/browserSetup.js
+++ src/pokemonEncounters/browserSetup.js
@@ -7,9 +7,61 @@ let encounterSixButton = document.getElementById("encounter-six");
 encounterOneButton.addEventListener('click', async () => {
 	let result = await getOnePokemon();
 	console.log(result);
+	result = [result];
+	localStorage.setItem("wildPokemon", JSON.stringify(result));
+
+	renderData();
+	// getOnePokemon().then(data => console.log(data));
 })
 
 encounterSixButton.addEventListener('click', async () => {
 	let result = await getMultiplePokemon();
 	console.log(result);
+	localStorage.setItem("wildPokemon", JSON.stringify(result));
+
+	renderData();
 })
+
+function renderData(){
+	// Retrieve data from local storage
+	let pokemonData = localStorage.getItem("wildPokemon");
+
+	console.log(pokemonData);
+	console.log(typeof(pokemonData));
+
+	pokemonData = JSON.parse(pokemonData);
+
+	// Reference to reuse
+	let wildPokemonContainer = document.getElementById("pokemon-encounters");
+	wildPokemonContainer.innerText = "";
+
+	pokemonData.forEach(pokemon => {
+		// Build HTML elements to display the data 
+		let newElement = buildPokemonDisplayElement(pokemon);
+
+		// Insert the new HTML elements into the current page 
+		wildPokemonContainer.appendChild(newElement);
+	});
+	
+}
+
+function buildPokemonDisplayElement(pokemonData){
+	if (!pokemonData){
+		return;
+	}
+
+	let pokemonContainer = document.createElement("div");
+
+	let pokemonHeading = document.createElement("h1");
+	pokemonHeading.innerText = pokemonData.name;
+	pokemonContainer.appendChild(pokemonHeading);
+
+	let pokemonImage = document.createElement("img");
+
+	console.log(pokemonData.sprites);
+
+	pokemonImage.src = pokemonData.sprites.front_default;
+	pokemonContainer.appendChild(pokemonImage);
+
+	return pokemonContainer;
+}
